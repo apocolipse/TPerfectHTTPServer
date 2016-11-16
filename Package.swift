f55eecdd82bc5158899fefe866ee09c3bd40309f
Chris Simpson <apocolipse@gmail.com>
2016-11-16 14:53:41 -0500

    Fix name

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-  name: "TZewoHTTPServer",
+  name: "TPerfectHTTPServer",
   dependencies: [
     .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git",
              majorVersion: 2, minor: 0),
