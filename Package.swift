fff022530c6b09a5255ad709c34bc13991cb2c61
Chris Simpson <apocolipse@gmail.com>
2017-02-01 23:22:22 -0500

    Update to PerfectHTTPServer 2.1, add logging layer

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 let package = Package(
   name: "TPerfectHTTPServer",
   dependencies: [
-    .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 0),
+    .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 1),
     .Package(url: "https://github.com/apocolipse/Thrift-Swift.git", majorVersion: 1, minor: 0),
-    .Package(url: "https://github.com/PerfectlySoft/Perfect-RequestLogger.git", majorVersion: 0),
+    .Package(url: "https://github.com/PerfectlySoft/Perfect-RequestLogger.git", majorVersion: 1),
   ]
 )
--- Sources/TPerfectHTTPServer.swift
+++ Sources/TPerfectHTTPServer.swift
@@ -12,81 +12,84 @@ import PerfectHTTPServer
 import Thrift
 import Foundation
 import PerfectRequestLogger
-import Dispatch
-
-let pQueue = DispatchQueue(label: "THttpServer.process.q", qos: .userInitiated, attributes: .concurrent)
-
+import PerfectLogger
 
 open class TPerfectHTTPServer<InProtocol: TProtocol, OutProtocol: TProtocol, Processor: TProcessor, Service> where Processor.Service == Service {
-
- private var server = HTTPServer()
- var serviceHandler: Service
-
- public init(address: String? = nil,
-             path: String? = nil,
-             port: Int,
-             service: Service,
-             inProtocol: InProtocol.Type,
-             outProtocol: OutProtocol.Type?=nil,
-             processor: Processor.Type) throws {
-
-   // set service handler
-   self.serviceHandler = service
-
-   if let address = address {
-     server.serverAddress = address
-   }
-   server.serverPort = UInt16(port)
-
-   var routes = Routes()
-   var uri = "/"
-   if let path = path {
-     uri += path
-   }
-   routes.add(method: .post, uri: uri) { request, response in
-     pQueue.async {
-       response.setHeader(.contentType, value: "application/x-thrift")
-
-       let itrans = TMemoryBufferTransport()
-       if let bytes = request.postBodyBytes {
-         let data = Data(bytes: bytes)
-         itrans.reset(readBuffer: data)
-       }
-
-       let otrans = TMemoryBufferTransport(flushHandler: { trans, buff in
-         let array = buff.withUnsafeBytes {
-           Array<UInt8>(UnsafeBufferPointer(start: $0, count: buff.count))
-         }
-         response.status = .ok
-         response.setBody(bytes: array)
-         response.completed()
-       })
-
-       let inproto = InProtocol(on: itrans)
-       let outproto = OutProtocol(on: otrans)
-
-       do {
-         let proc = Processor(service: self.serviceHandler)
-         try proc.process(on: inproto, outProtocol: outproto)
-         try otrans.flush()
-       } catch {
-         response.status = .badRequest
-         response.completed()
-       }
-     }
-   }
-   server.addRoutes(routes)
-   // Instantiate a logger
-   let myLogger = RequestLogger()
   
-   // Add the filters
-   // Request filter at high priority to be executed first
-   server.setRequestFilters([(myLogger, .high)])
-   // Response filter at low priority to be executed last
-   server.setResponseFilters([(myLogger, .low)])
- }
+  var serviceHandler: Service
+  var server: HTTPServer.Server? = nil
 
- public func serve() throws {
-   try server.start()
- }
+  public init(name: String = "", address: String = "localhost", path: String = "", port: Int = 9090,
+              service: Service, inProtocol: InProtocol.Type, outProtocol: OutProtocol.Type?=nil, processor: Processor.Type) throws {
+    
+    self.serviceHandler = service
+    // setup route
+    let route = Route(method: .post, uri: "/\(path)", handler: { request, response in
+      // logging helper
+      func log() {
+        let msgTuple = try? InProtocol(on: request.readTransport()).readMessageBegin()
+        LogFile.info("\(Date()) [\(msgTuple?.0 ?? "unknown")] from \(request.remoteAddress.host)")
+      }
+      
+      // set header
+      response.setHeader(.contentType, value: "application/x-thrift")
+      
+      // create protocols
+      let inProtocol = InProtocol(on: request.readTransport())
+      let outTransport = response.writeTransport()
+      let outProtocol = OutProtocol(on: outTransport)
+      log()
+      
+      do {
+        let proc = Processor(service: self.serviceHandler)
+        try proc.process(on: inProtocol, outProtocol: outProtocol)
+        try outTransport.flush() // sets response status/body
+        response.completed()
+      } catch {
+        response.status = .badRequest
+        response.completed()
+      }
+    })
+    
+    // serve it
+    server = HTTPServer.Server(name: name,
+                               address: address,
+                               port: port,
+                               routes: Routes([route]),
+                               requestFilters: [(RequestLogger(), HTTPFilterPriority.high)],
+                               responseFilters: [(RequestLogger(), HTTPFilterPriority.low)])
+
+    
+  }
+  
+  public func serve() throws {
+    if let server = server {
+      try HTTPServer.launch([server])
+    }
+  }
+}
+
+// Mark: Request/Response transport helpers
+
+extension HTTPRequest {
+  func readTransport() -> TMemoryBufferTransport {
+    let t = TMemoryBufferTransport()
+    if let data = self.postBodyBytes {
+      t.reset(readBuffer: Data(bytes: data))
+    }
+    return t
+  }
+}
+
+extension HTTPResponse {
+  func writeTransport() -> TMemoryBufferTransport {
+    let t = TMemoryBufferTransport(flushHandler: { trans, buff in
+      let array = buff.withUnsafeBytes {
+        Array<UInt8>(UnsafeBufferPointer(start: $0, count: buff.count))
+      }
+      self.status = .ok
+      self.setBody(bytes: array)
+    })
+    return t
+  }
 }
