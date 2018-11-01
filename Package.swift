e52d3d93c0c47744ada121f09fa876d8adbb7b5b
Alexander Zaporozhchenko <maxatma@mail.ua>
2018-11-01 21:48:25 +0200

    update to  PackageDescription API Version 4.2

--- Package.swift
+++ Package.swift
@@ -3,10 +3,20 @@
 import PackageDescription
 
 let package = Package(
-  name: "TPerfectHTTPServer",
-  dependencies: [
-    .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: "3.0.0"),
-    .package(url: "https://github.com/apocolipse/Thrift-Swift.git", from: "1.1.0")
-  ]
+    name: "TPerfectHTTPServer",
+    products: [
+        .library(name: "TPerfectHTTPServer", targets: ["TPerfectHTTPServer"]),
+        ],
+    dependencies: [
+        .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: "3.0.0"),
+        .package(url: "https://github.com/apocolipse/Thrift-Swift.git", from: "1.1.0"),
+    ],
+    targets: [
+        .target(
+            name: "TPerfectHTTPServer",
+            dependencies: ["PerfectHTTPServer", "Thrift"],
+            path: "Sources"),
+    ],
+    swiftLanguageVersions: [.v4_2]
 )
 
